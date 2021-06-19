78b8e790b2844e4e30c61d233e56e15561f7b319
Moisés <mguerolaferri2003@gmail.com>
2021-06-19 12:47:23 +0200

    Moises -> RegisterPage

--- lib/views/register.dart
+++ lib/views/register.dart
@@ -1,3 +1,9 @@
+import 'package:login/components/UIButton.dart';
+import 'package:login/components/UIField.dart';
+import 'package:login/components/UIHeader.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter/material.dart';
 import '../styles/styles.dart';
 
@@ -9,6 +15,90 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  /* -------------------------------------------------------------------------- */
+  /*                                DECLARATIONS                                */
+  /* -------------------------------------------------------------------------- */
+  late GlobalKey<FormState> _formKey;
+  late TextEditingController _fullNameController;
+  late TextEditingController _phoneController;
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  late TextEditingController _confirmPasswordController;
+  late FocusNode _fullNameFocus;
+  late FocusNode _phoneFocus;
+  late FocusNode _emailFocus;
+  late FocusNode _passwordFocus;
+  late FocusNode _confirmPasswordFocus;
+
+/* -------------------------------------------------------------------------- */
+/*                                   METHODS                                  */
+/* -------------------------------------------------------------------------- */
+  @override
+  void initState() {
+    super.initState();
+    _formKey = GlobalKey<FormState>();
+    _fullNameController = new TextEditingController();
+    _phoneController = new TextEditingController();
+    _emailController = new TextEditingController();
+    _passwordController = new TextEditingController();
+    _confirmPasswordController = new TextEditingController();
+    _fullNameFocus = new FocusNode();
+    _phoneFocus = new FocusNode();
+    _emailFocus = new FocusNode();
+    _passwordFocus = new FocusNode();
+    _confirmPasswordFocus = new FocusNode();
+  }
+
+  void unfocus() {
+    setState(() {
+      _fullNameFocus.unfocus();
+    });
+  }
+
+  void login() {
+    final sbNoData = SnackBar(
+      backgroundColor: Colors.red,
+      content: Text('Put data in the form inputs'),
+      duration: Duration(seconds: 2),
+      // action: SnackBarAction(
+      //   label: 'Close',
+      //   textColor: Colors.white,
+      //   onPressed: () {},
+      // ),
+    );
+
+    final sbNotMach = SnackBar(
+      backgroundColor: Colors.red,
+      content: Text('Passwords do not match'),
+      duration: Duration(seconds: 2),
+      // action: SnackBarAction(
+      //   label: 'Close',
+      //   textColor: Colors.white,
+      //   onPressed: () {},
+      // ),
+    );
+
+    if (!_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(sbNoData);
+      return;
+    }
+
+    if (_emailController.text.isEmpty ||
+        _fullNameController.text.isEmpty ||
+        _phoneController.text.isEmpty ||
+        _passwordController.text.isEmpty ||
+        _confirmPasswordController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(sbNoData);
+      return;
+    }
+
+    if (_passwordController != _confirmPasswordController) {
+      ScaffoldMessenger.of(context).showSnackBar(sbNotMach);
+    }
+
+    // STATE LOGGED
+  }
+
   void _back() {
     Navigator.of(context).pop();
   }
@@ -30,8 +120,76 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
               )
             ]),
-            Expanded(child: Center()),
+            Expanded(
+                child: Center(
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 500.0),
+                margin: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Center(child: _buildForm()),
+              ),
+            )),
           ],
         ));
   }
+
+  Widget _buildForm() => Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            UIHeader(
+              title: "Create Account",
+              description: "Please fill the input blow here",
+            ),
+            const SizedBox(height: 20.0),
+            UIField(
+              label: "Full Name",
+              icon: Icons.person_outline,
+              controller: _fullNameController,
+              focus: _fullNameFocus,
+              unfocus: unfocus,
+              isPassword: false,
+            ),
+            const SizedBox(height: 20.0),
+            UIField(
+              label: "Phone",
+              icon: Icons.phone_outlined,
+              controller: _phoneController,
+              focus: _phoneFocus,
+              unfocus: unfocus,
+              isPassword: false,
+            ),
+            const SizedBox(height: 20.0),
+            UIField(
+              label: "Email",
+              icon: Icons.email_outlined,
+              controller: _emailController,
+              focus: _emailFocus,
+              unfocus: unfocus,
+              isPassword: false,
+            ),
+            const SizedBox(height: 20.0),
+            UIField(
+              label: "Password",
+              icon: Entypo.key,
+              controller: _passwordController,
+              focus: _passwordFocus,
+              unfocus: unfocus,
+              isPassword: true,
+            ),
+            const SizedBox(height: 20.0),
+            UIField(
+              label: "Confirm Password",
+              icon: Entypo.key,
+              controller: _confirmPasswordController,
+              focus: _confirmPasswordFocus,
+              unfocus: unfocus,
+              isPassword: true,
+            ),
+            const SizedBox(height: 20.0),
+            UIButton(label: "Create Account", action: login),
+          ],
+        ),
+      );
 }
