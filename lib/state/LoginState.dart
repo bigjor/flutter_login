1da837dc0cb4e31b2a0def7261477ce24f71123a
Jordan <antenaEwok@gmail.com>
2021-06-23 17:09:51 +0200

    jordan -> añadido estado loggin a preferencias (no funcional)

--- lib/state/LoginState.dart
+++ lib/state/LoginState.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginState with ChangeNotifier, DiagnosticableTreeMixin {
-  bool _isLogged = false;
+  /* -------------------------------------------------------------------------- */
+  /*                           INSTANCES & CONSTRUCTOR                          */
+  /* -------------------------------------------------------------------------- */
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  LoginState() {
+    loadIsLogged();
+  }
 
+  /* -------------------------------------------------------------------------- */
+  /*                                DECLARATIONS                                */
+  /* -------------------------------------------------------------------------- */
+  bool _isLogged = false;
   bool get isLogged => _isLogged;
 
+  /* -------------------------------------------------------------------------- */
+  /*                                STATE METHODS                               */
+  /* -------------------------------------------------------------------------- */
   void login() {
     _isLogged = true;
+    saveIsLogged();
+
     notifyListeners();
   }
 
   void logout() {
     _isLogged = false;
+    saveIsLogged();
+
     notifyListeners();
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                PREFS METHODS                               */
+  /* -------------------------------------------------------------------------- */
+  void saveIsLogged() async {
+    final prefs = await _prefs;
+    prefs.setBool("isLogged", isLogged);
+  }
+
+  void loadIsLogged() async {
+    final prefs = await _prefs;
+    _isLogged = prefs.getBool("isLogged") ?? false;
   }
 
   /// Makes `LoginState` readable inside the devtools by listing all of its properties
